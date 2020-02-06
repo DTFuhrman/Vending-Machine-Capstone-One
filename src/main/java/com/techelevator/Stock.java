7f75a4ef68b75d0b502e53f2f1c0731946e70549
Jenna Tyzinski <jennatyzinski@gmail.com>
2020-02-06 16:51:07 -0500

    rrying to merge

--- src/main/java/com/techelevator/Stock.java
+++ src/main/java/com/techelevator/Stock.java
@@ -1,13 +1,34 @@
 package com.techelevator;
 
-public class Stock {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-	//read new stock file / current stock file
-	
-	//create current stock file
-	
-	//update current stock file
-	
-	//delete machine stock
-	
+public class Stock {
+	Map<String, Integer> machineProducts = new HashMap<String, Integer>();
+
+	// read new stock file / current stock file
+	public static void main(String[] args) throws IOException {
+		String fileName = "log.txt";
+		File baseStock = new File(fileName);
+		
+		//this will print to the new file. 
+		try (PrintWriter writer = new PrintWriter(baseStock)) {
+			String vendingMachine = "";
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Invalid Entry, Please Make Another Selection!");
+
+		}
+
+
+		// create current stock file
+
+		// update current stock file
+
+		// delete machine stock
+	}
 }
